2ba91acab7549fa0f9ad8f8995a949aba5166502
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-15 15:18:31 +0200

    p1: crea oggetto studente, console-log delle proprietà con ciclo for in

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
 
-alert("funzia?");
+  // PUNTO 1
+
+  // 1. Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età
+  var studente = {
+    "nome": "Gianni",
+    "cognome": "Ruggiero",
+    "eta": 45
+  }
+
+  // 2. Stampare a schermo attraverso il ciclo "for in" tutte le proprietà
+  console.log("PUNTO 1 ********************");
+  for (var key in studente) {
+    console.log(key);
+  }
 
 
 
