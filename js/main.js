87f6cd79dbe83ca5464805bcfbe962e9e2e4090e
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-15 15:22:10 +0200

    p2: crea array oggetti studenti, cicla e stampa nome e cognome studenti

--- js/main.js
+++ js/main.js
@@ -15,6 +15,52 @@ $(document).ready(function(){
     console.log(key);
   }
 
+  // PUNTO 2
+
+  // 1. Creare un array di oggetti di studenti
+
+  var studenti = [
+    {
+      "nome": "Gianni",
+      "cognome": "Ruggiero",
+      "eta": 45
+    },
+    {
+      "nome": "Pablo",
+      "cognome": "Nutini",
+      "eta": 25
+    },
+    {
+      "nome": "Eric",
+      "cognome": "Rossi",
+      "eta": 29
+    },
+    {
+      "nome": "Maria",
+      "cognome": "Fusi",
+      "eta": 22
+    },
+    {
+      "nome": "Oscar",
+      "cognome": "Bianchi",
+      "eta": 35
+    },
+    {
+      "nome": "Carla",
+      "cognome": "Rodriguez",
+      "eta": 23
+    }
+  ];
+
+  // 2. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+  console.log("PUNTO 2 ********************");
+  for (var i = 0; i < studenti.length; i++) {
+      console.log("Nome: " + studenti[i]["nome"]);
+      console.log("Cognome: " + studenti[i]["cognome"]);
+  }
+
+
+
 
 
 
